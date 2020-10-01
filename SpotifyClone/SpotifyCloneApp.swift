9a570d9da3438aadcb7060e7abb06ef604fa64c9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-01 21:29:35 +0300

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Ramill Ibragimov on 01.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
